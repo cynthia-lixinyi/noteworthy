1c44490d4aab5768ba0887de30374991bb85ecc4
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-01 16:48:44 -0700

    four links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,26 +5,10 @@ const NavBar = () => {
     return (
         <nav>
             <ul>
-                <li>
-                    <Link to="/">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/about">
-                        About us
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/demo">
-                        Demo
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/members">
-                        Members
-                    </Link>
-                </li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About us</Link></li>
+                <li><Link to="/demo">Demo</Link></li>
+                <li><Link to="/members">Members</Link></li>
             </ul>
         </nav>
     )
