a0ace6833ae254e134beb538c03235b4b0849f88
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-01 18:40:38 -0700

    Add logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './images/noteworthy-bear-logo.png';
+
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <nav>
+        <nav className="navbar">
+            <a href="/" className="logo">
+                <img src={logo} alt="logo" width="100" />
+            </a>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About us</Link></li>
