2b16164a309459bbf9845c9335edc5d51d7ac739
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-02 23:44:32 -0700

    Fix return statement in HomePage component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const HomePage = () => {
-    <div id="home">
-        <h1>Home Page</h1>
-    </div>
+    return (
+        <div id="home">
+            <h1>Home Page</h1>
+        </div>
+    )
 };
 
 export default HomePage;
