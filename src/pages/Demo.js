29616f9dd24678feba362e037d1c2156ec3a8be6
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-07 03:33:21 -0700

    [FEAT] carousel works on iframe videos, but repetitive items required in the video list

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import '../styles/Demo.css';
 import Slider from 'react-slick';
 import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'
-import { FlyerPictureList } from '../helpers/FlyerPictureList';
 
 const videos = [
-  <iframe src="https://www.youtube.com/embed/n0p8MxT8AxI?si=FdEKtCITnDXk3_5r" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>,
-  <iframe src="https://www.youtube.com/embed/kW57PpibCMA?si=iaMGvsddFC49tpTf" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>,
-  <iframe src="https://www.youtube.com/embed/GMnqKwlp9eg?si=UAF3WRgJlZV2lFnG" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  "https://www.youtube.com/embed/n0p8MxT8AxI?si=FdEKtCITnDXk3_5r",
+  "https://www.youtube.com/embed/kW57PpibCMA?si=iaMGvsddFC49tpTf",
+  "https://www.youtube.com/embed/GMnqKwlp9eg?si=UAF3WRgJlZV2lFnG",
+  "https://www.youtube.com/embed/n0p8MxT8AxI?si=FdEKtCITnDXk3_5r",
+  "https://www.youtube.com/embed/kW57PpibCMA?si=iaMGvsddFC49tpTf",
+  "https://www.youtube.com/embed/GMnqKwlp9eg?si=UAF3WRgJlZV2lFnG",
 ]
 
 const Demo = () => {
@@ -46,18 +48,11 @@ const Demo = () => {
         <h1>Our Favorite Videos</h1>
       </div>
       <div id="videos-carousel">
-        {/* <Slider {...settings}>
-          {videos.map( (item, idx) =>(
-            <div className={idx === videoIndex ? "slide activeSlide" : "slide" }>
-              {item}
-            </div>
-          ))}
-        </Slider> */}
         <Slider {...settings}>
-          {FlyerPictureList.map( (img, idx) => (
+          {videos.map( (video, idx) => (
             <div className={idx === videoIndex ? "slide activeSlide" : "slide"}>
-            <img src={img} alt={img} />
-          </div>
+              <iframe src={video} title={`Video ${idx}`} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
           ))}
         </Slider>
       </div>
