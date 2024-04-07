709ac0ff5eb3e10eb20802792324d7311ef72d82
Cynthia Xinyi Li <76521916+cynthia-lixinyi@users.noreply.github.com>
2024-04-07 15:15:52 -0700

    About hire us (#2)
    * Add content to Hire Us box on about us page
    
    * Finished hire us section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Flyer from '../components/Flyer';
 import '../styles/About.css';
+import hall from '../assets/sf-city-hall.jpg';
 
 const About = () => {
   return (
@@ -29,6 +30,17 @@ const About = () => {
         </div>
         <div className="HireUs">
           <h1>Hire Us</h1>
+          <p>Please e-mail our business manager at <b>noteworthy@ucchoral.berkeley.edu</b> and fill out this <a href="https://docs.google.com/forms/d/e/1FAIpQLSexPGp4LpkvAaE66Kmh5EllvAaXQtPmGquz_1dDo9g2PZ01hg/viewform" target="__blank">form</a>. 
+            Then we’ll respond to you with a quote as soon as possible.
+            In your email, let us know:
+            <ul>
+              <li>Who you are</li>
+              <li>The details of the event (where and when it'll be)</li>
+              <li>What kind of songs you'd like us to perform and how long the performance should be</li>
+            </ul>
+          </p>
+          <img id="hire-us-iamge" src={hall} alt='SF City Hall'/>
+          <p>For more details, please visit <a href="https://noteworthy.studentorg.berkeley.edu/hire-us/" target='__blank'>here</a></p>
         </div>
         <div className="FollowUs">
           <h1>Follow Us</h1>
