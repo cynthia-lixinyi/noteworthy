0a6c3c1df24614ba81d452f3dbb7435ba92c82ea
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-07 21:03:43 -0700

    Styled navbar and footer, slider doesn't work

--- src/components/Flyer.js
+++ src/components/Flyer.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Flyer.css';
+import Slider from 'react-slick';
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'
 
 import { FlyerPictureList } from '../helpers/FlyerPictureList';
 
 function Flyer() {
+
+  const NextArrow = ({onClick}) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
+      </div>
+    )
+  }
+
+  const PrevArrow = ({onClick}) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    )
+  }
+
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const settings = {
+    Infinite: true,
+    lazyLoad: true,
+    spped: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next)
+  }
+
   return (
-    <div className="flyer">
-      {FlyerPictureList.map((image) => {
-        return (
-          <img src={image} alt='event' />
-        )
-      })}
+    <div id="flyer">
+      <div id="image-carousel">
+        <Slider {...settings}>
+        {FlyerPictureList.map( (image, idx) => (
+          <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+            <img src={image} alt={`${idx}`}/>
+          </div>
+        ))}
+        </Slider>
+      </div>
     </div>
     
   )
